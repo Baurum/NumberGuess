d8ef5362ea4ab52731ffc300cfbe3d91ea7ae38a
Marcos <baurumtec@gmail.com>
2017-01-15 18:50:50 +0100

    Now the PC ask for your name

--- NumberGuess.java
+++ NumberGuess.java
@@ -12,10 +12,16 @@ public class NumberGuess {
 		Random rn = new Random();
 		int solution = rn.nextInt(101);	
 		String userInput = "";
+		System.out.println("What is your name? ");
+		String name = "";
+		Scanner reader = new Scanner(System.in);
+		name = reader.next(); 
+
+
+
 
 		while(1 == 1){
-			System.out.print("What number am I thinking? ");
-			Scanner reader = new Scanner(System.in);
+			System.out.print("What number am I thinking " + name +" ? ");
 			userInput = reader.next();
 			if(userInput.equals("quit")){
 				break;
@@ -23,7 +29,7 @@ public class NumberGuess {
 			int userGuess = Integer.parseInt(userInput);
 			int absUserGuess = Math.abs(userGuess - solution);
 			if(userGuess == solution){
-				System.out.println("(: CONGRATULATIONS!!! :')");
+				System.out.println("(: CONGRATULATIONS " + name +" :')");
 				break;
 			} else if(userGuess < solution && absUserGuess > 15){ 
 				System.out.println("COLD, you need to go much higher!!");
