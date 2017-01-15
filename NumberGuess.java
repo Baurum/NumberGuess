49931aefe7882d4ce85461328e23a68b4c702674
Marcos <baurumtec@gmail.com>
2017-01-15 19:45:14 +0100

    Final version with Score

--- NumberGuess.java
+++ NumberGuess.java
@@ -16,10 +16,9 @@ public class NumberGuess {
 		String name = "";
 		Scanner reader = new Scanner(System.in);
 		name = reader.next(); 
-		
-
-
-
+		int score = 100;
+		score -= 0 ;
+		System.out.	println("Initial Score: " + score);
 
 		while(1 == 1){
 			System.out.print("What number am I thinking " + name +" ? ");
@@ -31,16 +30,27 @@ public class NumberGuess {
 			int absUserGuess = Math.abs(userGuess - solution);
 			if(userGuess == solution){
 				System.out.println("(: CONGRATULATIONS " + name +" :')");
+
+				System.out.println("Your Score is: " + score);
 				break;
 			} else if(userGuess < solution && absUserGuess > 15){ 
+				score = score - 10;
 				System.out.println("COLD, you need to go much higher!!");
 			} else if(userGuess < solution && absUserGuess < 15){
+				score = score - 5;
 				System.out.println("HOT, you need to go a bit higher :)");
-			} else if(userGuess > solution && absUserGuess => 15){
+			} else if(userGuess > solution && absUserGuess >= 15){
+				score = score - 5;
 				System.out.println("COLD, not so high!!");
 			} else if(userGuess > solution && absUserGuess < 15){
+				score = score - 10;
 				System.out.println("HOT, you went only a bit too high ;)");
 			}
+			if(score <= 0){
+				System.out.println("You're so bad in this game, Bye " + name);
+				break;
+			}
+
 		}
 	}
 }
